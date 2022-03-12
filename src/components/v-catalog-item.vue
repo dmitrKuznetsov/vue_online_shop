280d0c4a24d1cd796afd3bffc21feb08531101a9
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-03-12 15:37:13 +0300

    add scss

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -5,6 +5,7 @@
         <button>Add to cart</button>
     </div>
 </template>
+
 <script>
 export default {
     name: "v-catalog-item",
@@ -15,6 +16,12 @@ export default {
     computed: {}
 }
 </script>
-<style>
-    
+
+<style lang="scss">
+    .v-catalog-item {
+        flex-basis: 25%;
+        box-shadow: 0 0 8px 0;
+        padding: $padding*2;
+        margin-bottom: $margin*2;
+    }
 </style>
